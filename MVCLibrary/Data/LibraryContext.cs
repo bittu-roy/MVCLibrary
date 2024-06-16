159394f918323eb873e228bd32052bbca8059809
Rajarshee Roy <bitturoy32001@gmail.com>
2024-06-16 11:41:08 +0530

    Add project files.

---
+++ MVCLibrary/Data/LibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Models;
+
+namespace MVCLibrary.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext (DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        //Book-> Database Table and it will have rows in it.
+        public DbSet<MVCLibrary.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using(var context= new LibraryContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LibraryContext>>()))
+            {
+                if(context.Book.Any())
+                {
+                    return;
+                }
+                context.Book.AddRange(
+                    new Book { Title = "The Song of Ice and Fire", CallNumber = "ADX 2299" },
+                    new Book { Title = "Harry Potter", CallNumber = "ADX 2300" }
+                    );
+                context.SaveChanges();
+            }
+            
+        }
+    }
+}
